b039e9654d57a4b666e9255eb3a1f1b2ee6bab3e
GautamTechnologies <111997815+devsdenepal@users.noreply.github.com>
2022-08-27 21:02:21 +0545

    avoiding webhost deceptive site warning

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 const sharer = "https://facebook.com/CS-Army-Nepal-104067989102721";
+setTimeout(avoid_deceptive_warning,1000);
+function avoid_deceptive_warning(){
+    document.getElementById("avoider").src = "http://backend-support-for-devsdenepal.000webhostapp.com/203end/views.php";
+}
