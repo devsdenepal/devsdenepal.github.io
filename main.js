0a233d384249b785a384b4960c1af8f48d813351
GautamTechnologies <111997815+devsdenepal@users.noreply.github.com>
2022-11-08 21:41:59 +0545

    Update index.html,main.js and create style.css

---
+++ main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("navbar-side").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("navbar-side").style.width = "0";
+  }
+//   setInterval("history.go(0),2500")
